a82937b80dabeed25eba4830570ea89f39ce10d9
Douglas Camargo <douglasresende@maxstudio.com.br>
2014-04-08 20:51:10 -0300

    Editing ViewController.m

--- CurrentClima/ViewController.m
+++ CurrentClima/ViewController.m
@@ -76,9 +76,7 @@
 -(void) carregarDados
 {
     NSDictionary *dadosCidadeEscolhida;
-    dadosCidadeEscolhida = [NSDictionary dictionaryWithContentsOfFile:[
-                                                       [NSBundle mainBundle] pathForResource:@"cidade_escolhida" ofType:@"plist"]
-                      ];
+    dadosCidadeEscolhida = [NSDictionary dictionaryWithContentsOfFile:filePath];
     [self.labelNomeCidade setText:[dadosCidadeEscolhida valueForKey:@"nome"]];
     [self.labelTemperaturaAtual setText:[dadosCidadeEscolhida valueForKey:@"temperatura"]];
     [self mudarImagemTemperatura:[dadosCidadeEscolhida valueForKey:@"temperatura"]];
@@ -109,75 +107,21 @@
 
 - (IBAction)atualizarDados:(id)sender
 {
-    NSMutableDictionary *novosDados;
-    [novosDados setObject:@"Rio de Janeiro" forKey:@"nome"];
-    if([novosDados writeToFile:[[NSBundle mainBundle] pathForResource:@"cidade_escolhida2" ofType:@"plist"] atomically:YES])
-    {
-        NSLog(@"Gravou");
-    } else {
-        NSLog(@"Nao gravou =(");
-    }
-    NSLog(@"%@", [[NSBundle mainBundle] pathForResource:@"cidade_escolhida2" ofType:@"plist"]);
-    
-    NSMutableDictionary* plistDict = [[NSMutableDictionary alloc] initWithContentsOfFile:@"cidade_escolhida2"];
-    [plistDict setObject:@"Rio de Janeiro" forKey:@"nome"];
-    
-    NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
-    NSLog(@"%@", paths);
-    NSString *documentsDirectory = [paths objectAtIndex:0];
-    NSLog(@"%@", documentsDirectory);
-    NSString *filePath = [documentsDirectory stringByAppendingPathComponent:@"cidade_escolhida2.plist"];
-    NSLog(@"%@", filePath);
-    
-    NSString* plistPath = nil;
-    NSFileManager* manager = [NSFileManager defaultManager];
-    if ((plistPath = [[[NSBundle mainBundle] bundlePath] stringByAppendingPathComponent:@"cidade_escolhida2.plist"]))
-    {
-        if ([manager isWritableFileAtPath:plistPath])
-        {
-            NSMutableDictionary* infoDict = [NSMutableDictionary dictionaryWithContentsOfFile:plistPath];
-            [infoDict setObject:@"blaaa" forKey:@"nome"];
-            [infoDict writeToFile:plistPath atomically:YES];
-            [manager setAttributes:[NSDictionary dictionaryWithObject:[NSDate date] forKey:NSFileModificationDate] ofItemAtPath:[[NSBundle mainBundle] bundlePath] error:nil];
-        }
-    }
-    
-    
-//    ------------------------------
-    
-    
-//    NSString *path = [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) lastObject];
-//    path = [path stringByAppendingPathComponent:@"cidade_escolhida2.plist"];
-    
-//    NSString *destPath = [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) lastObject];
-//    destPath = [destPath stringByAppendingPathComponent:@"cidade_escolhida2.plist"];
-//    
-//    // If the file doesn't exist in the Documents Folder, copy it.
-//    NSFileManager *fileManager = [NSFileManager defaultManager];
-//    
-//    if (![fileManager fileExistsAtPath:destPath]) {
-//        NSString *sourcePath = [[NSBundle mainBundle] pathForResource:@"cidade_escolhida2" ofType:@"plist"];
-//        [fileManager copyItemAtPath:sourcePath toPath:destPath error:nil];
-//    }
-    
-    
-//    - (void) rwDataToPlist:(NSString *)fileName playerColor:(NSString *)strPlayer withData:(NSArray *)data
-    
-    // Step1: Get plist file path
-    NSArray *sysPaths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory ,NSUserDomainMask, YES);
-    NSString *documentsDirectory2 = [sysPaths objectAtIndex:0];
-    NSString *filePath2 = [documentsDirectory2 stringByAppendingPathComponent:@"cidade_escolhida.plist"];
-    NSMutableDictionary *plistDict2;
-    if ([[NSFileManager defaultManager] fileExistsAtPath:filePath2])
+    NSMutableDictionary *plistDict;
+    if ([[NSFileManager defaultManager] fileExistsAtPath:filePath])
     {
         NSLog(@"Arquivo existe");
-        plistDict2 = [[NSMutableDictionary alloc] initWithContentsOfFile:filePath2];
+        plistDict = [[NSMutableDictionary alloc] initWithContentsOfFile:filePath];
     } else {
         NSLog(@"Arquivo Nao existe");
-        plistDict2 = [[NSMutableDictionary alloc] init];
-        [plistDict2 setValue:@"Rio de Janeiro" forKey:@"nome"];
+        plistDict = [[NSMutableDictionary alloc] init];
+        [plistDict setValue:@"Goiania" forKey:@"nome"];
+        [plistDict setValue:@"-22.91" forKey:@"latitude"];
+        [plistDict setValue:@"-43.18" forKey:@"longitude"];
+        [plistDict setValue:@"27" forKey:@"temperatura"];
+        [plistDict setValue:@"07/04/2014 18:29:35" forKey:@"data_atualizacao"];
         
-        BOOL didWriteToFile = [plistDict2 writeToFile:filePath2 atomically:YES];
+        BOOL didWriteToFile = [plistDict writeToFile:filePath atomically:YES];
         if (didWriteToFile)
         {
             NSLog(@"Write to .plist file is a SUCCESS!");
@@ -188,12 +132,7 @@
         }
     }
 
-    NSDictionary *listaCidades;
-    listaCidades = [NSDictionary dictionaryWithContentsOfFile:[
-                                                               [NSBundle mainBundle] pathForResource:@"cidade_escolhida" ofType:@"plist"]];
-//    NSLog(@"%@", [listaCidades valueForKey:@"nome"]);
-    NSLog(@"%@", [listaCidades description]);
-    
+    [self carregarDados];
 }
 
 
